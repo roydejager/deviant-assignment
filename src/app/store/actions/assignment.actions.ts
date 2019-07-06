cd45b20a243fecc78849a963eaa47728f73498aa
roydejager <roydejager@live.nl>
2019-07-06 22:34:03 +0200

    rename assignment actions

--- src/app/store/actions/assignment.actions.ts
+++ src/app/store/actions/assignment.actions.ts
@@ -2,23 +2,31 @@ import { Action } from '@ngrx/store';
 import { Assignment } from '../../models';
 
 export enum ActionTypes {
-  ASSIGNMENT_FETCH = 'Assignment fetch',
-  ASSIGNMENT_FETCH_SUCCESS = 'Assignment fetch success',
-  ASSIGNMENT_FETCH_ERROR = 'Assignment fetch error'
+  ASSIGNMENT_FETCH = '[Assignment] fetch',
+  ASSIGNMENT_FETCH_SUCCESS = '[Assignment] fetch success',
+  ASSIGNMENT_FETCH_ERROR = '[Assignment] fetch error',
+  ASSIGNMENT_FETCH_CANCEL = '[Assignment] fetch cancel'
 }
 
-export class FetchAssignment implements Action {
+export class AssignmentFetch implements Action {
   readonly type = ActionTypes.ASSIGNMENT_FETCH;
+
 }
 
-export class FetchAssignmentSuccess implements Action {
+export class AssignmentFetchSuccess implements Action {
   readonly type = ActionTypes.ASSIGNMENT_FETCH_SUCCESS;
 
   constructor(public payload: Assignment) { }
 }
 
-export class FetchAssignmentError implements Action {
+export class AssignmentFetchError implements Action {
   readonly type = ActionTypes.ASSIGNMENT_FETCH_ERROR;
+
+  constructor(public payload) { }
+}
+
+export class AssignmentFetchCancel implements Action {
+  readonly type = ActionTypes.ASSIGNMENT_FETCH_CANCEL;
 }
 
 export type AssignmentActions = ActionTypes;
